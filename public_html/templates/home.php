2fbe0e3f2dc6d9301056d03a7b806b510da3b1cc
Danielle Branch <dbranch82@gmail.com>
2017-08-24 11:24:50 -0600

    d

--- public_html/templates/home.php
+++ public_html/templates/home.php
@@ -1,1 +1,32 @@
-<h1>This is the Angular home page template.</h1>
+<div class="sfooter">
+	<div class="sfooter-content"></div>
+</div>
+<navbar></navbar>
+<body class="sfooter">
+	<div class="sfooter-content">
+
+
+		<main class="bg p-t-nav">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-8 col-md-offset-4">
+						<div class="jumbotron text-right">
+							<h1>Welcome to the Meow App!</h1>
+							<p class="lead">Now with 100% more fuzzy! =^. .^=</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</main>
+		<footer class="navbar-fixed-bottom">
+			<div class="container">
+				<div class="row">
+					<div class="col-sm-6">
+						=^. .^=
+					</div>
+					<div class="col-sm-6 text-right-sm">
+						Meow!
+					</div>
+				</div>
+			</div>
+		</footer>
